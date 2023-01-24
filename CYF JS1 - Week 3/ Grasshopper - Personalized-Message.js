a98ee2a4b4185cd4c8780841f563225b0889339d
AzinYad <azin.yadegari@gmail.com>
2023-01-24 21:41:35 +0000

    small enough

--- CYF JS1 - Week 3/ Grasshopper - Personalized-Message.js
+++ CYF JS1 - Week 3/ Grasshopper - Personalized-Message.js
@@ -10,3 +10,7 @@ function greet(name, owner) {
     // Add code here
     return name === owner ? 'Hello boss' : 'Hello guest';
 }
+
+// function greet(name, owner) {
+//     return `Hello ${name == owner ? 'boss' : 'guest'}`
+// }
